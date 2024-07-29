a659424a8e3742c8e611215b82ce313aeaf38e68
Th. Androutsos <a8anassis@gmail.com>
2024-07-29 15:21:01 +0300

    echo app

---
+++ src/gr/aueb/cf/sock/EchoServerApp.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.sock;
+
+public class EchoServerApp {
+
+    public static void main(String[] args) {
+        EchoServer echoServer = new EchoServer();
+        echoServer.start();
+    }
+}
